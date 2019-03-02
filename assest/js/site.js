33ebe48f873df21d2dce9df46b9c538e03817dc8
blueshysky <xionghuih745@gmail.com>
2019-03-02 16:33:21 -0600

    created the layout for the form, and made a alert on the js.

--- assest/js/site.js
+++ assest/js/site.js
@@ -1,12 +1,12 @@
 "use strict",
-
+function(){
 document.addEventListener("DOMContentLoaded", function(){
   console.log("Dom is loaded");
 
-  var headingtext = document.querySelector("#login");
+  var headingtext = document.querySelector("#signinsubmit");
   headingtext.addEventListener("click", function(){
   console.log("you clicked on this button");
-  window.prompt("signup!");
+  window.alert("You signed up signup! A notification has been sent out!");
 });
-
 });
+}();
